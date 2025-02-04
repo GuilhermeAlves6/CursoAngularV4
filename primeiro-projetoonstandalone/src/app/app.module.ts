3c8877aae2a9aa85ac32068b4f5c4a2ec04280a5
Guilherme Alves <Guilhermealves53139@gmail.com>
2025-02-04 00:01:49 -0300

    Curso Angular: Módulos (ngModule)

--- primeiro-projetoonstandalone/src/app/app.module.ts
+++ primeiro-projetoonstandalone/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './meu-primeiro/cursos/cursos.module';
 
 
 
@@ -16,7 +19,10 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
